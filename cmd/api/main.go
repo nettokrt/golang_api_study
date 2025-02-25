8b3af79aebb4db1ead77210ca596a4bd5981313a
Netto <marcos.freitasnetto15@gmail.com>
2025-02-24 21:21:11 -0300

    Add storage layer with Post and User management

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/nettokrt/golang_api_study/internal/env"
+	"github.com/nettokrt/golang_api_study/internal/store"
 )
 
 
@@ -12,10 +13,14 @@ func main()  {
 	cfg := config{
 		addr: env.GetString("ADDR",":8080"),
 	}
+	store := store.NewStorage(nil)
 	
 	app := &application{
 		config: cfg,
+		store: store,
 	}
+
+
 
 
 	mux := app.mount()
